4461f88049f4dad5eb702ff06b4a24ae607c303a
Andrew Caires <andrewcaires@live.com>
2022-10-07 02:39:36 -0300

    update

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,7 +1,7 @@
-import ui from "@andrewcaires/ui";
+import { ui } from "@andrewcaires/ui";
 import { App } from "vue";
 
-import "@andrewcaires/ui/src/scss/index.scss";
+import "@andrewcaires/ui/dist/style.css";
 
 export const install = (app: App) => app.use(ui);
 
