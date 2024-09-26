5dd0abf3a8af3a8b9e2be2f2a79eba8e8cb56b27
Andrew Caires <andrewcaires@live.com>
2024-09-25 21:59:31 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
